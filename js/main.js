c5ac0009472eef938e5fbce82da10b65157fd7c9
Isaiah Heavens <isaiahheavens@Isaiahs-MBP.fc.ca>
2020-01-17 11:08:47 -0500

    adding javascript to project

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ console.log('hello from JS!');
 
 // find the elements in the document
 let toggleButton = document.querySelector('.switch-text'),
-	myHeading = document.querySelector('h1');
-	svgImg = document.querySelector('#badge');
+	myHeading = document.querySelector('h1'),
+	//svgImg = document.querySelector('#badge'),
+	allSVGs = document.querySelectorAll(".svg");
 
 //this funtion should change the heading text
 function changeText() {
@@ -21,8 +22,13 @@ function logSVG() {
 	console.log(this.id);
 }
 
+// things a user can do to make stuff happen on the page
 toggleButton.addEventListener("click", changeText);
-svgImg.addEventListener("mouseover", logSVG);
+
+//svgImg.addEventListener("click", logSVG);
+//svgimg2.addEventListener("click", logSVG);
+
+allSVGs.forEach(item => item.addEventListener("click", logSVG));
 
 //document.querySelector('h1').textContent = "awesome website script"
 
